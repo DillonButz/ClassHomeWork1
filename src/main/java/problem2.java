9de2f9c642076b8fc50b5900b997e75624ee20ae
Djon Bon Junzi <Djon Bon Junzi@172.19.235.5>
2019-09-08 15:57:58 -0500

    Work Finished

--- src/main/java/problem2.java
+++ src/main/java/problem2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,6 +14,13 @@
 public class problem2 {
     public static void main(String[] args)
     {
+        float caloriesPerCookie = 75F;
+        Scanner makeNewScanner = new Scanner(System.in);
         
+        System.out.println("Please enter the number of cookies you ate: ");
+        float cookieInput = makeNewScanner.nextFloat();
+        float totalCookieCalories = caloriesPerCookie * cookieInput; 
+        
+        System.out.println("Total calories for the cookies you ate is " + totalCookieCalories);
     }
 }
